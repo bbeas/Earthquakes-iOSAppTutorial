ebe4ed84d585058807c2c2cb001941e5367299b6
bbeas <beibei_shen@outlook.com>
2023-06-08 14:49:04 +0100

    Adding the detail view

--- Earthquakes/Views/QuakeRow.swift
+++ Earthquakes/Views/QuakeRow.swift
@@ -1,15 +1,15 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+ See LICENSE folder for this sample’s licensing information.
 
-Abstract:
-The list item view which displays details of a given earthquake.
-*/
+ Abstract:
+ The list item view which displays details of a given earthquake.
+ */
 
 import SwiftUI
 
 struct QuakeRow: View {
     var quake: Quake
-    
+
     var body: some View {
         HStack {
             QuakeMagnitude(quake: quake)
@@ -30,7 +30,7 @@ struct QuakeRow_Previews: PreviewProvider {
                                     time: Date(timeIntervalSinceNow: -1000),
                                     code: "nc73649170",
                                     detail: URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc73649170.geojson")!)
-    
+
     static var previews: some View {
         QuakeRow(quake: previewQuake)
             .previewLayout(.sizeThatFits)
